3f0a651106c11dc93db910cd2f73409ed8a9cb60
Werisu <wellysson35@gmail.com>
2020-05-07 05:04:03 -0300

    autoload werisu classes

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    '' => array($vendorDir . '/werisu/php-classes/src'),
+    'Hcode\\' => array($vendorDir . '/werisu/php-classes/src'),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitb7e15e65016508ebd13b1f32d47d0e06
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/werisu/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/werisu/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInitb7e15e65016508ebd13b1f32d47d0e06
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInitb7e15e65016508ebd13b1f32d47d0e06::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitb7e15e65016508ebd13b1f32d47d0e06::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitb7e15e65016508ebd13b1f32d47d0e06::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInitb7e15e65016508ebd13b1f32d47d0e06::$prefixesPsr0;
             $loader->classMap = ComposerStaticInitb7e15e65016508ebd13b1f32d47d0e06::$classMap;
 
